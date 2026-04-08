e35abab2c5f319dfc9e801197896e984df8dcf94
Trevor Flahardy <trevorflahardy@gmail.com>
2026-04-07 20:45:02 -0400

    feat: add self-check-in functionality for user tickets with UI updates
    Closes #11

--- frontend/src/pages/EventDetail.tsx
+++ frontend/src/pages/EventDetail.tsx
@@ -219,6 +219,9 @@ export default function EventDetail() {
               fetchEvent={fetchEvent}
               bookingSuccess={bookingSuccess}
               bookingError={bookingError}
+              userTicket={userTicket}
+              checkingIn={checkingIn}
+              onCheckin={handleCheckin}
             />
 
             <EventAboutSection
